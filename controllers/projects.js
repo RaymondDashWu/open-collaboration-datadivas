d6546651fb74917b9ae6b277a3006ffd1284015c
RaymondDashWu <raymond31670@hotmail.com>
2018-11-19 22:18:03 -0800

    Added authentication template

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,16 +2,38 @@ const Project = require('../models/project');
 
 module.exports = (app) => {
 
-  // CREATE
-  app.post('/projects/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const project = new Project(req.body);
-
-    // SAVE INSTANCE OF POST MODEL TO DB
-    project.save((err, project) => {
-      // REDIRECT TO THE ROOT
-      return res.redirect(`/`);
+    app.get('/', function (req, res) {
+        Project.find({})
+            .then(projects => {
+                res.render("index.html");
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
     })
-  });
+
+    // CREATE
+    app.post('/projects/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const project = new Project(req.body);
+
+        // SAVE INSTANCE OF POST MODEL TO DB
+        project.save((err, project) => {
+            // REDIRECT TO THE ROOT
+            return res.redirect(`/`);
+        })
+    });
+
+    // TODO: Account for categories of projects
+    app.get("/projects/:category/:id", function (req, res) {
+        // LOOK UP THE POST
+        Project.findById(req.params.id)
+            .then(post => {
+                res.render("// TODO: Talk w/ Anna. DOES NOT EXIST YET");
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    });
 
 };
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-const projects = require('./controllers/projects.js')(app);
-const db = require('./data/platform-db.js');
-const Project = require('../models/project.js');
+// TOASK: OK to use var for these? Hoisted to top
+var express = require('express');
+var app = express();
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var projects = require('./controllers/projects.js')(app);
+var db = require('./data/platform-db.js');
+var Project = require('../models/project.js');
+var jwt = require('jsonwebtoken');
+
 
 // Use Body Parser
 app.use(bodyParser.json());
@@ -14,11 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
 
-
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('App listening on port 3000!')
