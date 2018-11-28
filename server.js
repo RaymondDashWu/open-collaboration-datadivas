d68226fbdae2ad48cad66af3a46bc2d32abe353f
Anna Pawl <annampawl@gmail.com>
2018-11-28 13:04:47 -0800

    debugging

--- server.js
+++ server.js
@@ -1,32 +1,8 @@
-const express = require('express');
-const app = express();
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-const exphbs = require('express-handlebars');
-const db = require('./data/platform-db.js');
-const Project = require('./models/project');
-const jwt = require('jsonwebtoken');
-const path = require('path');
+var express = require('express')
+var app = express()
 
-app.engine('handlebars', exphbs({defaultLayout: 'index'}));
-app.set('view engine', 'handlebars');
-
-
-// Use Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Add after body parser initialization!
-app.use(expressValidator());
-
-const projects = require('./controllers/projects.js')(app);
-
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-app.listen(process.env.PORT || 5000, () => {
-    console.log('App listening on port 5000!')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('App listening on port 3000!')
   })
+
+  
